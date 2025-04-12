acbc15c31fc2f6782fb62a0724c23167dee64cc0
tuobi555 <tuobi555@gmail.com>
2025-04-12 12:28:13 +0800

    更新

--- TextFSM_lab/textfsm_lab_fider.py
+++ TextFSM_lab/textfsm_lab_fider.py
@@ -2,20 +2,15 @@ from textfsm import TextFSM
 from pprint import pprint
 import os
 
-filepath = 'E:/桌面文件/配置模版/代码/TextFSM_lab/99/'
+filepath = 'E:/桌面文件/配置模版/代码/TextFSM_lab/99/'#文件们的路径
 filelist = os.listdir(filepath)#列出文件夹内的文件列表（包含子文件夹）
 #print(filelist)
+
 # 定义模板文件路径
-template_path = 'template2.text'
-
-# 打开模板文件并创建 TextFSM 模板对象
-
-
-
 for file in filelist:#循环filelist中的列表元素一次一个，赋值给file，下面代码的file代表着列表中每一个元素
-    with open(filepath + file) as f , open(template_path, 'r', encoding='utf-8') as template_file:
-        template = TextFSM(template_file)
-        raw_text_data = f.read()
+    with open(filepath + file) as f , open('template2.text', 'r', encoding='utf-8') as template_file:
+        template = TextFSM(template_file)#打开fsm的模板文件
+        raw_text_data = f.read()#逐个打开要提取的文件
         
         # 使用 TextFSM 解析文件内容
     result = template.ParseText(raw_text_data)
